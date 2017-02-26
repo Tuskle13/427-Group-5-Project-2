4a0987b49656e01d99e2a9e3c8ff177d92ec10eb
Tuskle13 <brit.nico@yahoo.com>
2017-02-26 00:18:18 -0600

    Basic Controls

---
+++ Project 2/Assets/PlayerContoller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour {
+
+    public float maxSpeed = 2f;
+    bool grounded = true;
+
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 500f;
+
+    Animator animator;
+    Rigidbody2D rigidBody;
+    SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    void Start() {
+        //animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        rigidBody.velocity = new Vector3(movement * maxSpeed, rigidBody.velocity.y);
+
+        if (movement < 0 && !spriteRenderer.flipX) Flip();
+        else if (movement > 0 && spriteRenderer.flipX) Flip();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    void Flip()
+    {
+        spriteRenderer.flipX = !spriteRenderer.flipX;
+    }
+}
