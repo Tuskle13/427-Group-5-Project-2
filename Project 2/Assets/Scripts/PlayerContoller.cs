794ad9deaa34415e3851947f9347b939bebcdca6
Tuskle13 <brit.nico@yahoo.com>
2017-04-04 14:19:02 -0500

    made an animation for level comletion

--- Project 2/Assets/Scripts/PlayerContoller.cs
+++ Project 2/Assets/Scripts/PlayerContoller.cs
@@ -12,20 +12,32 @@ public class PlayerContoller : MonoBehaviour {
     public Transform groundCheck;
     public LayerMask whatIsGround;
 
+    private bool stop = false;
+    private int count = 0;
+    private bool timeToDisco = false;
+
     Animator animator;
     Rigidbody2D rigidBody;
     SpriteRenderer spriteRenderer;
+    public GameObject discoBall;
+    Transform discoBallTransform;
+    SpriteRenderer discoBallSprite;
 
     // Use this for initialization
     void Start() {
         animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        discoBallTransform = discoBall.GetComponent<Transform>();
+        discoBallSprite = discoBall.GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
     {
-        float movement = Input.GetAxis("Horizontal");
+        float movement = 0;
+        if(!stop){
+            movement = Input.GetAxis("Horizontal");
+        }
         rigidBody.velocity = new Vector3(movement * maxSpeed, rigidBody.velocity.y);
 
         if (movement < 0 && !spriteRenderer.flipX) Flip();
@@ -39,10 +51,37 @@ public class PlayerContoller : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if(grounded && Input.GetKeyDown(KeyCode.Space))
+        if(!stop && grounded && Input.GetKeyDown(KeyCode.Space))
         {
             animator.SetBool("Grounded", false);
             rigidBody.AddForce(new Vector2(0, jumpForce));
+        }
+        else if (stop ){
+            if(discoBallTransform.localPosition.y > 1.45f){
+                float step = 2 * Time.deltaTime;
+                Vector3 position = discoBallTransform.position;
+                discoBallTransform.position = Vector3.MoveTowards(position, new Vector3(position.x,1.45f,position.z), step);
+            }else timeToDisco = true;
+
+            if(count % 10 == 0) discoBallSprite.flipX = !discoBallSprite.flipX;
+
+            if(timeToDisco && count == 25){
+                spriteRenderer.flipX = !spriteRenderer.flipX;
+                count = 0;
+            }else if (timeToDisco)
+                count++;
+
+            if(timeToDisco && !GetComponentInChildren<ParticleSystem>().isPlaying){
+                GetComponentInChildren<ParticleSystem>().Play();
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == "FinishLine"){
+            stop = true;
+            //GetComponentInChildren<ParticleSystem>().Play();
+            //GetComponent<ParticleSystem>().Play();
         }
     }
 
